21ce76ba1d43126c53034311042511f27b32433c
pseudoman21 <johnpaul.batusa@gmail.com>
2022-12-07 15:11:41 +0800

    Update dbank import

--- src/dbank_frontend/src/index.jsx
+++ src/dbank_frontend/src/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Greeting from "./Greeting";
+import DBankForm from "./Components/DBankForm";
+import '../assets/main.css'
 
 const App = () => {
  return (
-  <Greeting />
+  <DBankForm />
  )
 };
 
